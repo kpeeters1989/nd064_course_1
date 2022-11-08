378b3e456446441fb56bf9725dbd3e68f6c39f92
kpeeters1989 <117688112+kpeeters1989@users.noreply.github.com>
2022-11-08 10:58:21 +0100

    Update app.py

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -55,7 +55,7 @@ def index():
 @app.route('/healthz')
 def healthz():
     response = app.response_class(
-           response=json.dumps({"result: OK - healthy"}),
+           response=json.dumps({"result": "OK - healthy"}),
             status=200,
             mimetype='application/json'
     )
