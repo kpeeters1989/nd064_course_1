4751419acf8c1a2260ef1641cf010621460e0607
kpeeters1989 <117688112+kpeeters1989@users.noreply.github.com>
2022-11-09 12:21:34 +0100

    Update app.py

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -130,6 +130,5 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO,format='%(levelname)s:%(name)s %(message)s',  datefmt='%Y-%m-%d %H:%M:%S',handlers=handlers)
     logging.basicConfig(level=logging.ERROR,format='%(levelname)s:%(name)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S',handlers=handlers)
     app.run(host='0.0.0.0', port='3111')
-    app.run(host='0.0.0.0', port='3111')
     
 
